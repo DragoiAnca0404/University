e277b8b11899b3a5ad1d32cb6e9acac2166054ba
Adrian55Stack <adrianciurea112@yahoo.com>
2021-09-19 21:22:23 +0300

    Add project structure for all pages

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from '../shared/shared.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     SharedModule
   ],
-  exports: [LoginComponent]
+  exports: [HeaderComponent, FooterComponent]
 })
 export class MainModule { }
