72f88eba8608151179a9e93bc3ade4faec9f2bfa
Adrian55Stack <68397161+Adrian55Stack@users.noreply.github.com>
2021-10-16 21:07:41 +0300

    Add structure & errors for login form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,15 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  declarations: [
   ],
-  exports: [MatCardModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  exports: [FormsModule, MatCardModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
 })
 export class SharedModule { }
